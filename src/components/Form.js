d0380ef5b9f15d9843b1595bd777fe8e9197f161
StevenProgrammer01 <91440171+StevenProgrammer01@users.noreply.github.com>
2023-05-21 19:04:13 -0600

    changing a simple things

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 const API = process.env.REACT_APP_BACK;
 export const Form=(props)=>{
     //const [count, setCount] = useState(0);
